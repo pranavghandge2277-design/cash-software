92eeaa525cd2c0a3b86d5a86d7a964cbeee75ca5
Pranav <pranavghandge2277@gmail.com>
2026-05-06 02:55:14 -0700

    fix port for railway

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
-import app from './app'
+import app from './app';
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 })
-    console.log("Server running on http://localhost:3000")
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
-}
+    await app.listen({
+      port: Number(process.env.PORT) || 3000,
+      host: '0.0.0.0'
+    });
 
-start()
+    console.log("Server running 🚀");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+start();
